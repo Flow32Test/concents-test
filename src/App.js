e41e94a4a4d84e210c2a06502ca31d5ec99959d8
Flow32 Test <dev@flow32.de>
2021-08-05 22:46:55 +0200

    Match fonts Add new fonts Match styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from "react"
 import styled from "styled-components"
 
+import "./App.css"
+
 const Container = styled.div`
-  font-family: 'Kanit', sans-serif;
-  display: flex;
   align-items: center;
-  justify-content: center;
+  background-color: #fff;
+  display: flex;
+  font-family: 'SansPro', sans-serif;
   height: 100vh;
+  justify-content: center;
   width: 100%;
+`
+
+const HeadlineWrapper = styled.div`
+  padding: 50px;
+  border: 15px solid #000;
 `
 
 const Headline = styled.h1`
   font-size: 7.5em;
-  letter-spacing: 15px;
+  margin: 0;
   text-align: center;
+`
+
+const HeadlineBottomBorder = styled(Headline)`
+  border-bottom: 15px solid #000;
+  letter-spacing: 9px;
 `
 
 function App() {
   return (
     <Container>
-      <Headline>CONCENTS</Headline>
+      <HeadlineWrapper>
+        <HeadlineBottomBorder>CNCTNS</HeadlineBottomBorder>
+        <Headline>MNGMNT</Headline>
+      </HeadlineWrapper>
     </Container>
   )
 }
