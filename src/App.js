6415fdbccdb82f4c2fa3b249092173a9235d215a
Flow32 Test <dev@flow32.de>
2021-08-06 07:32:00 +0200

    Add theme toggle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from "react"
-import styled from "styled-components"
+import React, { useState } from "react"
+import styled, { ThemeProvider } from "styled-components"
 
 import "./App.css"
 
 const Container = styled.div`
   align-items: center;
-  background-color: #fff;
+  background-color: ${props => props.theme.background};
+  color: ${props => props.theme.font};
   display: flex;
   font-family: 'SansPro', sans-serif;
   height: 100vh;
   justify-content: center;
+  user-select: none;
   width: 100%;
 `
 
 const HeadlineWrapper = styled.div`
+  border: 15px solid ${props => props.theme.font};
+  cursor: pointer;
   padding: 50px;
-  border: 15px solid #000;
 `
 
 const Headline = styled.h1`
@@ -25,18 +28,36 @@ const Headline = styled.h1`
 `
 
 const HeadlineBottomBorder = styled(Headline)`
-  border-bottom: 15px solid #000;
+  border-bottom: 15px solid ${props => props.theme.font};
   letter-spacing: 9px;
 `
 
-function App() {
+const defaultTheme = {
+  background: "#fff",
+  font: "#000"
+}
+
+const invertTheme = {
+  font: "#fff",
+  background: "#000"
+}
+
+const App = () => {
+  const [isDefault, setIsDefault] = useState(true)
+
+  const toggle = () => {
+    setIsDefault(isDefault ? false : true)
+  }
+
   return (
-    <Container>
-      <HeadlineWrapper>
-        <HeadlineBottomBorder>CNCTNS</HeadlineBottomBorder>
-        <Headline>MNGMNT</Headline>
-      </HeadlineWrapper>
-    </Container>
+    <ThemeProvider theme={isDefault ? defaultTheme : invertTheme}>
+      <Container>
+        <HeadlineWrapper onClick={toggle}>
+          <HeadlineBottomBorder>CNCTNS</HeadlineBottomBorder>
+          <Headline>MNGMNT</Headline>
+        </HeadlineWrapper>
+      </Container>
+    </ThemeProvider>
   )
 }
 
